3acf5e9942bca7ff9bcb1d064e1bc5a44c4a15c7
ronaldobergara <bergaraheadbanger@gmail.com>
2021-11-29 13:05:14 -0300

    Algumas funções utilizando string

---
+++ Mais_do_que_strings.py
@@ -0,0 +1,21 @@
+nome = 'kaori bergara'
+
+print(nome.encode())
+
+print(nome.upper())
+
+print(nome.capitalize())
+
+print(nome.title())
+
+print(nome.replace('k', 'KK'))
+
+print(nome.split('a'))
+
+# para concatenar utilize join
+print(' '.join(['Eslen', 'Pires', 'Bergara']))
+
+# quebrar linha com join
+print('\n'.join(['Eslen', 'Pires', 'Bergara']))
+
+print(len(nome))
