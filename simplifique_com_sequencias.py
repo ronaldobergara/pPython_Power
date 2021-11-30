dc4700bcad86dd1e163342aa2045d0149153c530
ronaldobergara <bergaraheadbanger@gmail.com>
2021-11-29 23:20:57 -0300

    slice

---
+++ simplifique_com_sequencias.py
@@ -0,0 +1,28 @@
+nome = 'ronaldo'
+# pega o ultimo elemento
+print(nome[-1])
+
+# pega a primeira ate a quarta letra
+print(nome[0:4])
+
+# pega a segunda e a penultima
+print(nome[1:-1])
+
+# pega da segunda ate a ultima
+print(nome[1:])
+
+# pega a primeira ate a quarta letra
+print(nome[:4])
+
+# tudo
+print(nome[:])
+
+# pula de 2 em 2
+print(nome[::2])
+
+# pula de 2 em 2
+print(nome[1:7:2])
+
+# com negativo
+print(nome[::-1])
+
